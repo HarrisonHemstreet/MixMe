8ea6b171e9d627d4576023eb795a6e292c25ef64
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-04-03 22:10:34 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// THIS APP IS BUILT TO ALLOW USERS TO TWEET "@mixme" AND THEN MIXME WILL RESPOND WITH A TWEET WITH A LINK TO THEIR PERSONALIZED
+// SPOTIFY PLAYLIST
+
+// Listen for tweets that tweet at @mixme
+// 2. When a user MENTIONS @mixme, get the tweet's id
+// 3. IF the tweet's id IS NOT already in the database, then ADD the tweet id to the database, AND reply to the tweet with SPOTIFY info
+
+    
