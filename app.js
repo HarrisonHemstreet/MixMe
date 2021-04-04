91131df4e1e5d425b6d519f71d3289d99954d98b
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-04-03 22:57:16 -0600

    creating server

--- app.js
+++ app.js
@@ -5,4 +5,14 @@
 // 2. When a user MENTIONS @mixme, get the tweet's id
 // 3. IF the tweet's id IS NOT already in the database, then ADD the tweet id to the database, AND reply to the tweet with SPOTIFY info
 
-    
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json())
+
+
+app.listen(5000, () => {
+    console.log("Server has started on port 5000!");
+})
